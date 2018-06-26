9a364774e69040b28e01c50e566ac50be8763a59
Almost Engineer <bitsecondal@gmail.com>
2018-06-25 20:45:32 -0500

    #3 Added discount

--- src/com/twitter/almostengr/javacalculator/SalesTax.java
+++ src/com/twitter/almostengr/javacalculator/SalesTax.java
@@ -10,12 +10,26 @@ public class SalesTax {
 	private static double taxRate;
 	private static double amount;
 	private static double salesTotal;
+	private static double discountdollar = 0;
+	private static double discountPercent = 0;
 	
+	public static double getDiscountdollar() {
+		return discountdollar;
+	}
+	public static void setDiscountdollar(double discountdollar) {
+		SalesTax.discountdollar = discountdollar;
+	}
+	public static double getDiscountPercent() {
+		return discountPercent;
+	}
+	public static void setDiscountPercent(double discountPercent) {
+		SalesTax.discountPercent = discountPercent;
+	}
 	public static double getTaxRate() {
 		return taxRate;
 	}
 	public static void setTaxRate(double taxRate) {
-		SalesTax.taxRate = taxRate;
+		SalesTax.taxRate = taxRate / 100;
 	}
 	public static double getAmount() {
 		return amount;
@@ -34,9 +48,16 @@ public class SalesTax {
 		// sales tax formula
 		// total = amount + (1 + taxrate)
 		
-		double total = amount * (1 + taxRate);
+		double totalDiscount = amount * (1 - discountPercent);
+		double total = (amount * (1 + taxRate));
+		
+		if (discountPercent > 0) {
+			total = total - totalDiscount;
+		}
 		
 		setAmount(total);
 		System.out.println("Sales Total: " + total);
 	}
 }
+
+
