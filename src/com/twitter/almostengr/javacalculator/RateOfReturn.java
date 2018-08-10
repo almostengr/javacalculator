c5601d72ae4a5ae93cfcb2db2c135e13b59cd304
Almost Engineer <bitsecondal@gmail.com>
2018-08-10 05:41:44 -0500

    Closed #5 Calculate percentage increase. Added comments

--- src/com/twitter/almostengr/javacalculator/RateOfReturn.java
+++ src/com/twitter/almostengr/javacalculator/RateOfReturn.java
@@ -3,17 +3,31 @@ package com.twitter.almostengr.javacalculator;
 public class RateOfReturn {
 	private static double initialValue; 
 	private static double finalValue;
-	private static double rate;
 	
+	/**
+	 * Performs the rate of return calculation
+	 */
 	public static void calcRateOfReturn() {
+		double rate;
+		double percentageRate;
+		
 		rate = (finalValue - initialValue) / initialValue;
-		System.out.println("Rate of Return: " + rate);
+		percentageRate = rate * 100;
+		System.out.println("Rate of Return: " + percentageRate + "%");
 	}
 
+	/**
+	 * Sets the starting value 
+	 * @param initialValue The first value
+	 */
 	public static void setInitialValue(double initialValue) {
 		RateOfReturn.initialValue = initialValue;
 	}
 
+	/**
+	 * Sets the final value
+	 * @param finalValue The last value
+	 */
 	public static void setFinalValue(double finalValue) {
 		RateOfReturn.finalValue = finalValue;
 	}
